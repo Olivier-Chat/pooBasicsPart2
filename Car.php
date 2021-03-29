8749d45ff966c7d551736e6421785ed46f33a12a
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-03-29 12:49:05 +0200

    exercice complete

--- Car.php
+++ Car.php
@@ -1,64 +1,37 @@
 <?php
-class Car {
-    private int $nbWheels;
-    private int $currentSpeed;
-    private string $color;
-    private int $nbSeats;
+
+require_once 'Vehicle.php';
+class Car extends Vehicle
+{
     private string $energy;
     private int $energyLevel;
 
-    public function __construct(string $color,int $nbSeats,string $energy)
-    {
-        $this->color = $color;
-        $this->nbSeats = $nbSeats;
-        $this->energy = $energy;
-    }
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+        ];
 
-    public function start ():string
+    public function __construct(string $color, int $nbSeats, string $energy)
     {
-        $this->currentSpeed = 5;
-        return "Let's get started";
-    }
-    public function forward ():string
-    {
-        $this->currentSpeed = 30;
-        return "Go!";
-    }
-
-    public function brake(): string
-    {
-        $sentence = "";
-        while ($this->currentSpeed > 0) {
-            $this->currentSpeed--;
-            $sentence .= "Brake !!!";
-        }
-        $sentence .= "I'm stopped !";
-        return $sentence;
-    }
-
-    public function getNbWheels(): int
-    {
-        return $this->nbWheels;
-    }
-
-    public function getNbSeats(): int
-    {
-        return $this->nbSeats;
-    }
-    public function getColor(): string
-    {
-        return $this->color;
+        parent::__construct($color,$nbSeats);
+        $this->setEnergy($energy);
     }
     public function getEnergy(): string
     {
         return $this->energy;
     }
+    public function setEnergy(string $energy): void
+    {
+        if(in_array($energy,self::ALLOWED_ENERGIES)){
+            $this->energy = $energy;
+        }
+    }
     public function getEnergyLevel(): int
     {
         return $this->energyLevel;
     }
-    public function getCurrentSpeed(): int
+    public function setEnergyLevel(int $energyLevel): void
     {
-        return $this->currentSpeed;
+        $this->energyLevel = $energyLevel;
     }
 }
--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
-require 'Car.php';
-require 'Bicycle.php';
+require_once 'Truck.php';
+require_once 'Car.php';
+$monster = new Truck('red',3,Truck::ALLOWED_ENERGIES[0],100);
+var_dump($monster).PHP_EOL;
+echo $monster->forward().PHP_EOL;
+echo $monster->brake().PHP_EOL;
+echo $monster->controlFilling().PHP_EOL;
+$monster -> setShipping(50);
+echo $monster->controlFilling().PHP_EOL;
+$monster -> setShipping($monster->getStorageCapacity());
+echo $monster->controlFilling().PHP_EOL;
 
-$myBike1 = new Bicycle('red');
-$myBike2 = new Bicycle('red');
-$myBike3 = new Bicycle('red');
-$myCar1 = new Car('blue',4,"fuel");
-$myCar2 = new Car('blue',4,"fuel");
-$myCar3 = new Car('blue',4,"fuel");
-$myCar4 = new Car('blue',4,"fuel");
-
-
-echo'My bike2: '.$myBike2->forward()."my Current speed is: ".$myBike2->getCurrentSpeed().' km/h'.PHP_EOL;
-echo'My car4: '.$myCar4->forward()."my Current speed is: ".$myCar4->getCurrentSpeed().' km/h'.PHP_EOL;
-
-echo'My bike2: '.$myBike2->brake().PHP_EOL;
-echo'My car4: '.$myCar4->brake().PHP_EOL;
